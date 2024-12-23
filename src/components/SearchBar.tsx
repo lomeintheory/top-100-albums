0292e8f4176f05e20b73662fb8a8a6f82068d19c
Andrew Lo <al@iMac.lan>
2024-12-22 22:30:25 -0800

    create search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+interface SearchBarProps {
+  onSearch: (searchTerm: string, searchCriteria: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchCriteria, setSearchCriteria] = useState('title');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(searchTerm, searchCriteria);
+  };
+
+  return (
+    <form className='mb-4' onSubmit={handleSubmit}>
+      <div className='input-group'>
+        <select
+          className='form-select'
+          value={searchCriteria}
+          onChange={e => setSearchCriteria(e.target.value)}>
+          <option value='title'>Title</option>
+          <option value='artist'>Artist</option>
+          <option value='genre'>Genre</option>
+        </select>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Search albums...'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button className='btn btn-primary' type='submit'>
+          Search
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
