76b0da658961e2ee0ee502940253c2b41aa38493
Andrew Lo <al@iMac.lan>
2024-12-22 16:17:39 -0800

    refactor pagination feature to pagination component

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AlbumCard from './AlbumCard';
+import Pagination from './Pagination';
 
 const AlbumList = () => {
   const [albums, setAlbums] = useState([]);
@@ -22,14 +23,6 @@ const AlbumList = () => {
   const startIndex = (currentPage - 1) * 8;
   const paginatedAlbums = albums.slice(startIndex, startIndex + 8); // current 8 of total albums displayed on each page
 
-  // const handleNext = () => {
-  //   currentPage < totalPages ? setCurrentPage(prev => prev + 1) : totalPages;
-  // };
-
-  // const handlePrevious = () => {
-  //   currentPage > 1 ? setCurrentPage(prev => prev - 1) : 1;
-  // };
-
   const handlePageClick = (pageNumber: number) => {
     const page = Math.max(1, Math.min(pageNumber, totalPages));
     setCurrentPage(page);
@@ -50,40 +43,11 @@ const AlbumList = () => {
           </div>
         ))}
       </div>
-      <nav className='d-flex justify-content-center'>
-        <ul className='pagination'>
-          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-            <button
-              className='page-link'
-              onClick={() => handlePageClick(currentPage - 1)}>
-              Previous
-            </button>
-          </li>
-          {Array.from({ length: totalPages }).map((_, index) => (
-            <li
-              className={`page-item ${
-                currentPage === index + 1 ? 'active' : ''
-              }`}
-              key={index}>
-              <button
-                className='page-link'
-                onClick={() => handlePageClick(index + 1)}>
-                {index + 1}
-              </button>
-            </li>
-          ))}
-          <li
-            className={`page-item ${
-              currentPage === totalPages ? 'disabled' : ''
-            }`}>
-            <button
-              className='page-link'
-              onClick={() => handlePageClick(currentPage + 1)}>
-              Next
-            </button>
-          </li>
-        </ul>
-      </nav>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageClick={handlePageClick}
+      />
     </div>
   );
 };
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageClick: (pageNumber: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageClick,
+}) => {
+  return (
+    <nav className='d-flex justify-content-center'>
+      <ul className='pagination'>
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <button
+            className='page-link'
+            onClick={() => onPageClick(currentPage - 1)}>
+            Previous
+          </button>
+        </li>
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <li
+            className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}
+            key={index}>
+            <button
+              className='page-link'
+              onClick={() => onPageClick(index + 1)}>
+              {index + 1}
+            </button>
+          </li>
+        ))}
+        <li
+          className={`page-item ${
+            currentPage === totalPages ? 'disabled' : ''
+          }`}>
+          <button
+            className='page-link'
+            onClick={() => onPageClick(currentPage + 1)}>
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
