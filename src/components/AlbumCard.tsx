473876806db225a6ef8de0937e299f50fc1984a7
Andrew Lo <al@iMac.lan>
2024-12-22 20:33:12 -0800

    make albums clickable

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { Album } from './AlbumList';
 
 interface AlbumCardProps {
+  album: Album;
   title: string;
   artist: string;
   cover: string;
   releaseDate: string;
+  onAlbumClick: (album: Album) => void;
 }
 
 const AlbumCard: React.FC<AlbumCardProps> = ({
+  album,
   title,
   artist,
   cover,
   releaseDate,
+  onAlbumClick,
 }) => {
   return (
-    <div className='album-card'>
+    <div
+      className='album-card'
+      onClick={() => onAlbumClick(album)}
+      style={{ cursor: 'pointer' }}>
       <img src={cover} alt={title} />
       <h2>{title}</h2>
       <p>{artist}</p>
