cae354f191066843106a81663a7861394a3820e0
Andrew Lo <al@iMac.lan>
2024-12-26 17:18:25 -0800

    fix imports and edit gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.scss';
+import './styles/styles.scss';
 import 'bootstrap';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
